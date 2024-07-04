387eed4f9df9aee09221bb99da5f4f06f63cdd27
lihuiling <337655347@qq.com>
2024-07-04 18:12:57 +0800

    feat:添加gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hl-Interview-questions/" /* 基础虚拟路径: */,
+  dest: "dist" /* 打包文件基础路径, 在命令所在目录下 */,  
+  title: "面试题", // 标题
   plugins: [vue()],
   optimizeDeps: {
   }
